5c20ae971485ac8499b9e84f3d19e14974c0dfe4
Robbie Jaeger <jaeger.rob@gmail.com>
2022-04-20 10:37:39 -0600

    Move character to obstacle

--- scripts.js
+++ scripts.js
@@ -2,13 +2,31 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 init();
+// character
+let character = {height: 100, width: 100}
 
-let coordinate = 0;
+// obstacle
+let obstacle = {x: 600, height: 200, width: 20};
+
+let stepLength = 10;
+let characterCoordinate = 0;
+let viewPort = 0;
+let totalPath = 0;
 
 document.addEventListener('keydown', function(event){
   if (event.keyCode === 39) {
-    coordinate++;
+    if (characterCoordinate <= (canvas.width/2)) {
+      characterCoordinate = characterCoordinate + stepLength;
+    } else if (totalPath === (obstacle.x - character.width)) {
+      alert("Here's a question");
+    } else {
+      viewPort = viewPort + stepLength;
+    }
+    totalPath = totalPath + stepLength;
   }
+  console.log("Character:", characterCoordinate);
+  console.log("Viewport:", viewPort);
+  console.log("Total Path:", totalPath);
 })
 
 function init() {
@@ -19,6 +37,10 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
   ctx.fillStyle = 'green';
-  ctx.fillRect(coordinate, (canvas.height - 110), 100, 100);
+  ctx.fillRect(characterCoordinate, (canvas.height - 110), character.width, character.height);
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect((obstacle.x - viewPort), (canvas.height - obstacle.height), obstacle.width, obstacle.height);
+
   window.requestAnimationFrame(draw);
 }
