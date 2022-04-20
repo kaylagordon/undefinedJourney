946655b47e2901d466bf2e3e2fe87350c07a74c9
Jeremiah Black <jeremiahblackol@gmail.com>
2022-04-20 11:21:00 -0600

    game asks question and continues to move after answer

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
+var questionBox = document.querySelector('.question-box');
+var definition = document.querySelector('.definition');
+var listItem1 = document.querySelector('.li1');
+var listItem2 = document.querySelector('.li2');
+var listItem3 = document.querySelector('.li3');
+
+
+
+
 init();
 // character
 let character = {height: 100, width: 100}
@@ -12,22 +21,44 @@ let stepLength = 10;
 let characterCoordinate = 0;
 let viewPort = 0;
 let totalPath = 0;
+var newQuestion;
 
 document.addEventListener('keydown', function(event){
-  if (event.keyCode === 39) {
+  if (event.keyCode === 39 && questionBox.classList.contains('hidden')) {
     if (characterCoordinate <= (canvas.width/2)) {
       characterCoordinate = characterCoordinate + stepLength;
     } else if (totalPath === (obstacle.x - character.width)) {
-      alert("Here's a question");
+      newQuestion = new Question();
+      newQuestion.createQuestion()
+      questionBox.classList.remove('hidden')
+      definition.innerText = newQuestion.definition
+      listItem1.innerText = newQuestion.answerChoices[0]
+      listItem2.innerText = newQuestion.answerChoices[1]
+      listItem3.innerText = newQuestion.answerChoices[2]
     } else {
       viewPort = viewPort + stepLength;
     }
     totalPath = totalPath + stepLength;
-  }
-  console.log("Character:", characterCoordinate);
-  console.log("Viewport:", viewPort);
-  console.log("Total Path:", totalPath);
+  } else if (event.keyCode === 49 && !questionBox.classList.contains('hidden')) {
+    if (newQuestion.checkAnswer(listItem1.innerText)) {
+        console.log("Correct")
+    } else {
+        console.log("Incorrect!")
+    }
+    questionBox.classList.add("hidden")
+  } else if (event.keyCode === 50 && !questionBox.classList.contains('hidden')) {
+      
+  } else if (event.keyCode === 51 && !questionBox.classList.contains('hidden')) {
+
+      
+}
+
 })
+
+
+
+//console log the question
+// 
 
 function init() {
   window.requestAnimationFrame(draw);
