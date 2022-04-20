c0e17f340cbf29bcc555bd510db04aa52bed28d2
Jeremiah Black <jeremiahblackol@gmail.com>
2022-04-20 11:51:08 -0600

    fixing bug where character doesnt meet trigger

--- scripts.js
+++ scripts.js
@@ -14,8 +14,8 @@ init();
 // character
 let character = {height: 100, width: 100}
 
-// obstacle
-let obstacle = {x: 600, height: 200, width: 20};
+// questionTrigger
+let questionTrigger = {x: 600, height: 200, width: 20};
 
 let stepLength = 10;
 let characterCoordinate = 0;
@@ -25,9 +25,13 @@ var newQuestion;
 
 document.addEventListener('keydown', function(event){
   if (event.keyCode === 39 && questionBox.classList.contains('hidden')) {
+      totalPath = totalPath + stepLength;
     if (characterCoordinate <= (canvas.width/2)) {
       characterCoordinate = characterCoordinate + stepLength;
-    } else if (totalPath === (obstacle.x - character.width)) {
+    } else if (totalPath === (questionTrigger.x - character.width + stepLength)) {
+        console.log("stepLength", stepLength)
+        console.log("total path", totalPath)
+        console.log("question trigger", questionTrigger.x)
       newQuestion = new Question();
       newQuestion.createQuestion()
       questionBox.classList.remove('hidden')
@@ -38,7 +42,10 @@ document.addEventListener('keydown', function(event){
     } else {
       viewPort = viewPort + stepLength;
     }
-    totalPath = totalPath + stepLength;
+    if (questionTrigger.x === ((totalPath - stepLength - questionTrigger.width) - (canvas.width / 2))) {
+        questionTrigger.x += 1000 
+        
+    }
   } else if (event.keyCode === 49 && !questionBox.classList.contains('hidden')) {
     if (newQuestion.checkAnswer(listItem1.innerText)) {
         console.log("Correct")
@@ -80,7 +87,7 @@ function draw() {
   ctx.fillRect(characterCoordinate, (canvas.height - 110), character.width, character.height);
 
   ctx.fillStyle = 'red';
-  ctx.fillRect((obstacle.x - viewPort), (canvas.height - obstacle.height), obstacle.width, obstacle.height);
+  ctx.fillRect((questionTrigger.x - viewPort), (canvas.height - questionTrigger.height), questionTrigger.width, questionTrigger.height);
 
   window.requestAnimationFrame(draw);
 }
