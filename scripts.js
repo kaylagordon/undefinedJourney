ae1e21bacc52e79d93f7a18ad98adb7ebc358b52
Robbie Jaeger <jaeger.rob@gmail.com>
2022-04-20 12:07:51 -0600

    Fix increasing question point

--- scripts.js
+++ scripts.js
@@ -7,9 +7,6 @@ var listItem1 = document.querySelector('.li1');
 var listItem2 = document.querySelector('.li2');
 var listItem3 = document.querySelector('.li3');
 
-
-
-
 init();
 // character
 let character = {height: 100, width: 100}
@@ -25,13 +22,13 @@ var newQuestion;
 
 document.addEventListener('keydown', function(event){
   if (event.keyCode === 39 && questionBox.classList.contains('hidden')) {
-      totalPath = totalPath + stepLength;
+      totalPath += stepLength;
+      console.log("total path", totalPath)
+      console.log("question trigger", questionTrigger.x)
+      console.log("character coordinate", characterCoordinate)
     if (characterCoordinate <= (canvas.width/2)) {
-      characterCoordinate = characterCoordinate + stepLength;
-    } else if (totalPath === (questionTrigger.x - character.width + stepLength)) {
-        console.log("stepLength", stepLength)
-        console.log("total path", totalPath)
-        console.log("question trigger", questionTrigger.x)
+      characterCoordinate += stepLength;
+    } else if (totalPath === (questionTrigger.x - character.width)) {
       newQuestion = new Question();
       newQuestion.createQuestion()
       questionBox.classList.remove('hidden')
@@ -43,8 +40,8 @@ document.addEventListener('keydown', function(event){
       viewPort = viewPort + stepLength;
     }
     if (questionTrigger.x === ((totalPath - stepLength - questionTrigger.width) - (canvas.width / 2))) {
-        questionTrigger.x += 1000 
-        
+        questionTrigger.x += 1000
+        totalPath -= stepLength;
     }
   } else if (event.keyCode === 49 && !questionBox.classList.contains('hidden')) {
     if (newQuestion.checkAnswer(listItem1.innerText)) {
@@ -59,14 +56,14 @@ document.addEventListener('keydown', function(event){
     } else {
         console.log("Incorrect!")
     }
-    questionBox.classList.add("hidden")    
+    questionBox.classList.add("hidden")
   } else if (event.keyCode === 51 && !questionBox.classList.contains('hidden')) {
     if (newQuestion.checkAnswer(listItem3.innerText)) {
         console.log("Correct")
     } else {
         console.log("Incorrect!")
     }
-    questionBox.classList.add("hidden")    
+    questionBox.classList.add("hidden")
 }
 
 })
@@ -74,7 +71,7 @@ document.addEventListener('keydown', function(event){
 
 
 //console log the question
-// 
+//
 
 function init() {
   window.requestAnimationFrame(draw);
